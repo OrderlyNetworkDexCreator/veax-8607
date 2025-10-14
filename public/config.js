c286b027251a305762015d7e2bac1e54d9704794
Mario Reder <mario.reder@pm.me>
2025-10-14 10:36:35 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "veaxdex",
   "VITE_ORDERLY_BROKER_NAME": "VEAX",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "true",
